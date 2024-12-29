c175cc85c57d9908e30121618f709e877e6f371b
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-29 16:44:11 +0300

    Create "EfBlogDal" ... GenericRepository<Blog>, IBlogDal ..

---
+++ Organic.DataAccessLayer/EntityFramework/EfBlogDal.cs
@@ -0,0 +1,19 @@
+﻿using Organic.DataAccessLayer.Abstract;
+using Organic.DataAccessLayer.Context;
+using Organic.DataAccessLayer.Repositories;
+using Organic.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organic.DataAccessLayer.EntityFramework
+{
+    public class EfBlogDal : GenericRepository<Blog>, IBlogDal
+    {
+        public EfBlogDal(OrganicContext context) : base(context)
+        {
+        }
+    }
+}
