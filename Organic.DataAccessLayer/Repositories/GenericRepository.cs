f856fa078ec20bceb5f46319867605f28eac9ace
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-29 16:29:51 +0300

    CTRL + . "Generate constructor" private readonly OrganicContext _context;

--- Organic.DataAccessLayer/Repositories/GenericRepository.cs
+++ Organic.DataAccessLayer/Repositories/GenericRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Organic.DataAccessLayer.Abstract;
+using Organic.DataAccessLayer.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,12 @@ namespace Organic.DataAccessLayer.Repositories
 {
     public class GenericRepository<T> : IGenericDal<T> where T : class
     {
+        private readonly OrganicContext _context;
+        public GenericRepository(OrganicContext context)
+        {
+            _context = context;
+        }
+
         public void Delete(T entity)
         {
             throw new NotImplementedException();
