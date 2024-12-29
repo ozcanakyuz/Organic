afabdbb59a322d5f50317d4b6cdf54477c2c99de
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-29 12:43:20 +0300

    Create SocialMedia.cs Added SocialMediaId, Title, Url..

---
+++ Organic.EntityLayer/Concrete/SocialMedia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organic.EntityLayer.Concrete
+{
+    public class SocialMedia
+    {
+        public int SocialMediaId { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+
+    }
+}
