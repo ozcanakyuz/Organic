aaae9ba1ce4f9b36fbe5418ea548f7684f01abdc
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-29 16:37:31 +0300

    Create "IAboutDal" IGenericDal<About>

---
+++ Organic.DataAccessLayer/Abstract/IAboutDal.cs
@@ -0,0 +1,13 @@
+﻿using Organic.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organic.DataAccessLayer.Abstract
+{
+    public interface IAboutDal : IGenericDal<About>
+    {
+    }
+}
