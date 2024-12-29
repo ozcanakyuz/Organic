628ce6c1537db55c21a24e7dd425cb977e4671d7
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-29 12:49:40 +0300

    Created Blog.cs BlogId, Title, CreatedAt..

---
+++ Organic.EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organic.EntityLayer.Concrete
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string CategoryTitle { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Description { get; set; }
+    }
+}
