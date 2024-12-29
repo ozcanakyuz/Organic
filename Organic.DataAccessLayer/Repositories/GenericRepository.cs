0e4402bc95f71ee60f4b38453e2daa2b468cb2cf
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-29 16:28:27 +0300

    Implement Interface GenericRepository<T> : IGenericDal<T> where T : class

--- Organic.DataAccessLayer/Repositories/GenericRepository.cs
+++ Organic.DataAccessLayer/Repositories/GenericRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Organic.DataAccessLayer.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace Organic.DataAccessLayer.Repositories
 {
-    internal class GenericRepository
+    public class GenericRepository<T> : IGenericDal<T> where T : class
     {
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
