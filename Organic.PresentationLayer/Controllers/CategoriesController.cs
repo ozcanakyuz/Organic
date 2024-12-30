687b57b40fd972cd79314b9e9f5cf04f18df43b5
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-30 17:16:29 +0300

    Create "CategoriesController" private readonly ICategoryService _categoryService; Index() -> CategoryList(): Right click > Add View.. >Use a layout page : AdminLayout>index.cshtml

---
+++ Organic.PresentationLayer/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Organic.BusinnesLayer.Abstract;
+
+namespace Organic.PresentationLayer.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult CategoryList()
+        {
+            var values = _categoryService.TGetAll();
+            return View(values);
+        }
+    }
+}
