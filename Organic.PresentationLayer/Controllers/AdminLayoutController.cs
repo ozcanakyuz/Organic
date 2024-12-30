58468fc68a07bb2110be8f3a1f1bfd7e6eca3de9
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-30 14:41:18 +0300

    Controllers Add>Controller "AdminLayoutController"

---
+++ Organic.PresentationLayer/Controllers/AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Organic.PresentationLayer.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
