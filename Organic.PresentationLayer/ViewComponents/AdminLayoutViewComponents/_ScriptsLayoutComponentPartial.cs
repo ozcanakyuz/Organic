014c66750cff517e2888a2f13843768c2a66c5f0
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-30 17:09:40 +0300

    _FooterLayoutComponentPartial _ScriptsLayoutComponentPartial

---
+++ Organic.PresentationLayer/ViewComponents/AdminLayoutViewComponents/_ScriptsLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Organic.PresentationLayer.ViewComponents.AdminLayoutViewComponents
+{
+    public class _ScriptsLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
