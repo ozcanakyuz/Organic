ab6b95ccc32b431bf7c3c7bd3f1265483d4f1b42
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-30 14:37:34 +0300

    Create "SocialMediaManager" SocialMediaManager : ISocialMediaService private readonly ISocialMediaDal _socialMediaDal;

---
+++ Organic.BusinnesLayer/Concrete/SocialMediaManager.cs
@@ -0,0 +1,45 @@
+﻿using Organic.BusinnesLayer.Abstract;
+using Organic.DataAccessLayer.Abstract;
+using Organic.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organic.BusinnesLayer.Concrete
+{
+    public class SocialMediaManager : ISocialMediaService
+    {
+        private readonly ISocialMediaDal _socialMediaDal;
+        public SocialMediaManager(ISocialMediaDal socialMediaDal)
+        {
+            _socialMediaDal = socialMediaDal;
+        }
+
+        public void TDelete(SocialMedia id)
+        {
+            _socialMediaDal.Delete(id);
+        }
+
+        public List<SocialMedia> TGetAll()
+        {
+            return _socialMediaDal.GetAll();
+        }
+
+        public SocialMedia TGetById(int id)
+        {
+            return _socialMediaDal.GetById(id);
+        }
+
+        public void TInsert(SocialMedia entity)
+        {
+            _socialMediaDal.Insert(entity);
+        }
+
+        public void TUpdate(SocialMedia entity)
+        {
+            _socialMediaDal.Update(entity);
+        }
+    }
+}
