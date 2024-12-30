b822bb4884e4242c887f56a50c9f1feed0d6f51c
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-30 14:32:13 +0300

    Create "ProductManager" ProductManager : IProductService private readonly IProductDal _productDal;

---
+++ Organic.BusinnesLayer/Concrete/ProductManager.cs
@@ -0,0 +1,45 @@
+﻿using Organic.BusinnesLayer.Abstract;
+using Organic.DataAccessLayer.Abstract;
+using Organic.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organic.BusinnesLayer.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private readonly IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void TDelete(Product id)
+        {
+            _productDal.Delete(id);
+        }
+
+        public List<Product> TGetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public Product TGetById(int id)
+        {
+            return _productDal.GetById(id);
+        }
+
+        public void TInsert(Product entity)
+        {
+            _productDal.Insert(entity);
+        }
+
+        public void TUpdate(Product entity)
+        {
+            _productDal.Update(entity);
+        }
+    }
+}
