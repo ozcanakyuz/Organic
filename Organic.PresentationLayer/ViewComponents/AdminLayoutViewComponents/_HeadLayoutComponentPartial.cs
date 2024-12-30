bdf0fbb569e4cc29fabd10568c1ebd3552da430a
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-30 14:58:59 +0300

    Create Folder "ViewComponents" Add "AdminLayoutViewComponents" -> new Class "_HeadLayoutComponentPartial" Views>Shared Create Folder "_HeadLayoutComponentPartial" -> Right Clik "Add View" -> "Razor View" -> Name: "Default" Options: "Create as a partial view" Add click.

---
+++ Organic.PresentationLayer/ViewComponents/AdminLayoutViewComponents/_HeadLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Organic.PresentationLayer.ViewComponents.AdminLayoutViewComponents
+{
+    public class _HeadLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
