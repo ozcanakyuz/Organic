7fdaba7008c45a7a1533f133773ed0c7d8118eaa
OZCAN AKYUZ <akyzozcan@gmail.com>
2024-12-31 16:11:15 +0300

    Database Diagrams; Categories(CategoryId)<-->Products(ProductId)

--- Organic.DataAccessLayer/Migrations/20241231130510_mig4.Designer.cs
+++ Organic.DataAccessLayer/Migrations/20241231130510_mig4.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Organic.DataAccessLayer.Context;
 
@@ -11,9 +12,11 @@ using Organic.DataAccessLayer.Context;
 namespace Organic.DataAccessLayer.Migrations
 {
     [DbContext(typeof(OrganicContext))]
-    partial class OrganicContextModelSnapshot : ModelSnapshot
+    [Migration("20241231130510_mig4")]
+    partial class mig4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Organic.EntityLayer/Concrete/Category.cs
+++ Organic.EntityLayer/Concrete/Category.cs
@@ -11,5 +11,6 @@ namespace Organic.EntityLayer.Concrete
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
     }
 }
