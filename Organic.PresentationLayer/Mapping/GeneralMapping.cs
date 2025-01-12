c2f278a49a6d49054d521dbb14399f6dc9d41174
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-12 09:51:31 +0300

    Mapping folder -> using AutoMapper;

---
+++ Organic.PresentationLayer/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Organic.DtoLayer.Dtos.AboutDtos;
+using Organic.DtoLayer.Dtos.SliderDtos;
+using Organic.EntityLayer.Concrete;
+
+namespace Organic.PresentationLayer.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<ResultSliderDto, Slider>().ReverseMap();
+            CreateMap<CreateSliderDto, Slider>().ReverseMap();
+            CreateMap<UpdateSliderDto, Slider>().ReverseMap();
+            CreateMap<GetByIdSliderDto, Slider>().ReverseMap();
+
+            CreateMap<ResultAboutDto, About>().ReverseMap();
+            CreateMap<CreateAboutDto, About>().ReverseMap();
+            CreateMap<UpdateAboutDto, About>().ReverseMap();
+            CreateMap<GetByIdAboutDto, About>().ReverseMap();
+        }
+    }
+}
