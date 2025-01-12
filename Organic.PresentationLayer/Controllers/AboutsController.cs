050796f60f36fc4101730d7ec3279344e436874b
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-12 11:05:26 +0300

    AboutList

---
+++ Organic.PresentationLayer/Controllers/AboutsController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Organic.BusinnesLayer.Abstract;
+using Organic.DtoLayer.Dtos.AboutDtos;
+
+namespace Organic.PresentationLayer.Controllers
+{
+    public class AboutsController : Controller
+    {
+        private readonly IAboutService _aboutService;
+        private readonly IMapper _mapper;
+
+        public AboutsController(IAboutService aboutService, IMapper mapper)
+        {
+            _aboutService = aboutService;
+            _mapper = mapper;
+        }
+
+        public IActionResult AboutList()
+        {
+            var values = _aboutService.TGetAll();
+            return View(_mapper.Map<List<ResultAboutDto>>(values));
+        }
+        
+    }
+}
