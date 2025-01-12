8ede6d19d5e0ed2352a8b209ea2c72de874f2ec8
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-12 19:29:42 +0300

    _DefaultSecondBannerSectionComponentPartial _DefaultSecondSliderSectionComponentPartial

---
+++ Organic.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultSecondSliderSectionComponentPartial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Organic.BusinnesLayer.Abstract;
+using System.Diagnostics.Contracts;
+
+namespace Organic.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultSecondSliderSectionComponentPartial : ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public _DefaultSecondSliderSectionComponentPartial(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _productService.TGetAll();
+            return View(values);
+        }
+    }
+}
