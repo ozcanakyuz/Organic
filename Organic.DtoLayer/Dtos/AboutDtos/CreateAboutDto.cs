d263410894b83b185050446735d13f3c69c67659
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-12 09:46:50 +0300

    AboutDto OK

---
+++ Organic.DtoLayer/Dtos/AboutDtos/CreateAboutDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organic.DtoLayer.Dtos.AboutDtos
+{
+    public class CreateAboutDto
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+    }
+}
