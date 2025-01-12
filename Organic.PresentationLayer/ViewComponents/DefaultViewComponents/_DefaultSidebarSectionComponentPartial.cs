08b265bf7dbf20625e716ffcbae3f06f3591291f
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-12 13:23:21 +0300

    _DefaultNavbarSectionComponentPartial _DefaultSidebarSectionComponentPartial

---
+++ Organic.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultSidebarSectionComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Organic.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultSidebarSectionComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
