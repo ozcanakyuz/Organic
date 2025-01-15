c015102b53cfd856168e7a5ee555d46d1c4fb98e
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-15 21:39:33 +0300

    _DefaultAboutSectionComponentPartial

---
+++ Organic.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultAboutSectionComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Organic.BusinnesLayer.Abstract;
+
+namespace Organic.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultAboutSectionComponentPartial : ViewComponent
+    {
+        private readonly IAboutService _aboutService;
+
+        public _DefaultAboutSectionComponentPartial(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _aboutService.TGetAll();
+            return View(values);
+        }
+    }
+}
