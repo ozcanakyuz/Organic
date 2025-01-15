0ad0af8115f2f05165248c8a9b9e0e2e87648396
OZCAN AKYUZ <akyzozcan@gmail.com>
2025-01-15 18:46:07 +0300

    _DefaultFooterSectionComponentPartial

---
+++ Organic.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultFooterSectionComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Organic.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultFooterSectionComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
